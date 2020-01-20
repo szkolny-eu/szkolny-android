3b0570d21cf12e372e94d79d4f87676a4f4f6007
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2020-01-20 23:10:35 +0100

    Revert "[Sync] Lower the priority of sync notification."
    This reverts commit 1677be9e6fa30478564df94201ea0f8193d6e5b8.

--- app/src/main/java/pl/szczodrzynski/edziennik/data/api/EdziennikNotification.kt
+++ app/src/main/java/pl/szczodrzynski/edziennik/data/api/EdziennikNotification.kt
@@ -23,9 +23,6 @@ class EdziennikNotification(val app: App) {
         NotificationCompat.Builder(app, ApiService.NOTIFICATION_API_CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_notification)
                 .setPriority(PRIORITY_MIN)
-                .setDefaults(0)
-                .setSound(null)
-                .setVibrate(longArrayOf(0))
                 .setOngoing(true)
                 .setLocalOnly(true)
     }
