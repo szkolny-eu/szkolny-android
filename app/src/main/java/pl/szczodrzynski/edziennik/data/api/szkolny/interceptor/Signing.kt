da48c059ec641717cbd0f6cba6f4d42089b0ea18
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2020-03-30 23:29:34 +0200

    [4.0-rc.4] Update build.gradle, signing and changelog.

--- app/src/main/java/pl/szczodrzynski/edziennik/data/api/szkolny/interceptor/Signing.kt
+++ app/src/main/java/pl/szczodrzynski/edziennik/data/api/szkolny/interceptor/Signing.kt
@@ -46,6 +46,6 @@ object Signing {
 
     /*fun provideKey(param1: String, param2: Long): ByteArray {*/
     fun pleaseStopRightNow(param1: String, param2: Long): ByteArray {
-        return "$param1.MTIzNDU2Nzg5MDZhgrg9J9===.$param2".sha256()
+        return "$param1.MTIzNDU2Nzg5MDj3yyZoD8===.$param2".sha256()
     }
 }
