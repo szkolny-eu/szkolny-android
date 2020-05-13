b13257cb786d9c19ca1781163092bebdce8df61b
Kacper Ziubryniewicz <kapi2289@gmail.com>
2020-05-13 17:33:42 +0200

    [API/Podlasie] Add getting grades.

--- app/src/main/java/pl/szczodrzynski/edziennik/data/api/edziennik/podlasie/data/api/PodlasieApiMain.kt
+++ app/src/main/java/pl/szczodrzynski/edziennik/data/api/edziennik/podlasie/data/api/PodlasieApiMain.kt
@@ -24,6 +24,7 @@ class PodlasieApiMain(override val data: DataPodlasie,
             data.getTeam() // Save the class team when it doesn't exist.
 
             json.getJsonArray("Timetable")?.asJsonObjectList()?.let { PodlasieApiTimetable(data, it) }
+            json.getJsonArray("Marks")?.asJsonObjectList()?.let { PodlasieApiGrades(data, it) }
 
             data.setSyncNext(ENDPOINT_PODLASIE_API_MAIN, SYNC_ALWAYS)
             onSuccess(ENDPOINT_PODLASIE_API_MAIN)
