47557710631c9308b36bf587480b8c279dfe123a
Pengwius <tomekftomekf@gmail.com>
2021-04-06 13:50:37 +0200

    Contributors item in settings

---
+++ app/src/main/java/pl/szczodrzynski/edziennik/ui/modules/contributors/ContributorsActivity.kt
@@ -0,0 +1,12 @@
+package pl.szczodrzynski.edziennik.ui.modules.contributors
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pl.szczodrzynski.edziennik.R
+
+class ContributorsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contributors)
+    }
+}
