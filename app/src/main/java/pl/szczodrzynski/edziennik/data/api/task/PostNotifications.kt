1452461c0701da6369a0021856f24ce4de3e7fb0
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2021-04-09 13:32:37 +0200

    [Notifications] Update icon colors.

--- app/src/main/java/pl/szczodrzynski/edziennik/data/api/task/PostNotifications.kt
+++ app/src/main/java/pl/szczodrzynski/edziennik/data/api/task/PostNotifications.kt
@@ -8,7 +8,6 @@ import android.util.SparseIntArray
 import androidx.core.app.NotificationCompat
 import androidx.core.util.forEach
 import androidx.core.util.set
-import com.mikepenz.iconics.IconicsColor
 import com.mikepenz.iconics.IconicsDrawable
 import com.mikepenz.iconics.typeface.library.community.material.CommunityMaterial
 import com.mikepenz.iconics.utils.*
@@ -129,9 +128,10 @@ class PostNotifications(val app: App, nList: List<AppNotification>) {
                     .setContentText(buildSummaryText(summaryCounts))
                     .setTicker(newNotificationsText)
                     .setSmallIcon(R.drawable.ic_notification)
-                    .setLargeIcon(IconicsDrawable(app, CommunityMaterial.Icon.cmd_bell_ring_outline)
-                        .color { IconicsColor.colorRes(R.color.md_white_1000) }
-                        .toBitmap())
+                    .setLargeIcon(IconicsDrawable(app).apply {
+                        icon = CommunityMaterial.Icon.cmd_bell_ring_outline
+                        colorRes = R.color.colorPrimary
+                    }.toBitmap())
                     .setStyle(NotificationCompat.InboxStyle()
                             .also {
                                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
@@ -162,9 +162,9 @@ class PostNotifications(val app: App, nList: List<AppNotification>) {
                         .setSubText(if (it.type == TYPE_SERVER_MESSAGE) null else it.title)
                         .setTicker("${it.profileName}: ${it.title}")
                         .setSmallIcon(R.drawable.ic_notification)
-                        .setLargeIcon(getLargeIcon(it.type)
-                            .color { IconicsColor.colorRes(R.color.colorPrimary) }
-                            .toBitmap())
+                        .setLargeIcon(getLargeIcon(it.type).apply {
+                            colorRes = R.color.colorPrimary
+                        }.toBitmap())
                         .setStyle(NotificationCompat.BigTextStyle()
                                 .bigText(it.text))
                         .setWhen(it.addedDate)
@@ -188,9 +188,10 @@ class PostNotifications(val app: App, nList: List<AppNotification>) {
                         .setContentText(buildSummaryText(summaryCounts))
                         .setTicker(newNotificationsText)
                         .setSmallIcon(R.drawable.ic_notification)
-                        .setLargeIcon(IconicsDrawable(app, CommunityMaterial.Icon.cmd_bell_ring_outline)
-                            .color { IconicsColor.colorRes(R.color.md_white_1000) }
-                            .toBitmap())
+                        .setLargeIcon(IconicsDrawable(app).apply {
+                            icon = CommunityMaterial.Icon.cmd_bell_ring_outline
+                            colorRes = R.color.colorPrimary
+                        }.toBitmap())
                         .addDefaults()
                         .setGroupSummary(true)
                         .setContentIntent(summaryIntent)
