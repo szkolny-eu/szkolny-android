1e8fb6a9aecaaff1f5da8cb5fb621d2563d5d144
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2021-04-14 20:19:04 +0200

    [UI/Messages] Restore search string after closing a message. Add message counter.

--- app/src/main/java/pl/szczodrzynski/edziennik/ui/modules/messages/models/MessagesSearch.kt
+++ app/src/main/java/pl/szczodrzynski/edziennik/ui/modules/messages/models/MessagesSearch.kt
@@ -5,7 +5,5 @@
 package pl.szczodrzynski.edziennik.ui.modules.messages.models
 
 class MessagesSearch {
-    var isFocused = false
-    var searchText = ""
-    var count = 0
+    var searchText: CharSequence = ""
 }
