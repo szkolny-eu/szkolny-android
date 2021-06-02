bd499f4c0ba9b26aa95204d22ee2cfa5b0767b63
BxOxSxS <BxOxSxS@protonmail.com>
2021-06-02 20:03:52 +0200

    [API/Librus] Update client ID

--- app/src/main/java/pl/szczodrzynski/edziennik/data/api/Constants.kt
+++ app/src/main/java/pl/szczodrzynski/edziennik/data/api/Constants.kt
@@ -24,7 +24,7 @@ const val FAKE_LIBRUS_ACCOUNTS = "/synergia_accounts.php"
 
 val LIBRUS_USER_AGENT = "${SYSTEM_USER_AGENT}LibrusMobileApp"
 const val SYNERGIA_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Gecko/20100101 Firefox/62.0"
-const val LIBRUS_CLIENT_ID = "0RbsDOkV9tyKEQYzlLv5hs3DM1ukrynFI4p6C1Yc"
+const val LIBRUS_CLIENT_ID = "VaItV6oRutdo8fnjJwysnTjVlvaswf52ZqmXsJGP"
 const val LIBRUS_REDIRECT_URL = "app://librus"
 const val LIBRUS_AUTHORIZE_URL = "https://portal.librus.pl/oauth2/authorize?client_id=$LIBRUS_CLIENT_ID&redirect_uri=$LIBRUS_REDIRECT_URL&response_type=code"
 const val LIBRUS_LOGIN_URL = "https://portal.librus.pl/rodzina/login/action"
