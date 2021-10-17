9d3ec18d93c6c08364c734b76bb39b0d7dd474c0
Antoni Czaplicki <56671347+Antoni-Czaplicki@users.noreply.github.com>
2021-10-17 21:17:07 +0200

    Update app/src/main/java/pl/szczodrzynski/edziennik/data/db/entity/Teacher.kt
    Co-authored-by: Kuba Szczodrzyński <kuba@szczodrzynski.pl>

--- app/src/main/java/pl/szczodrzynski/edziennik/data/db/entity/Teacher.kt
+++ app/src/main/java/pl/szczodrzynski/edziennik/data/db/entity/Teacher.kt
@@ -147,8 +147,6 @@ open class Teacher {
         this.profileId = profileId
         this.id = id
     }
-
-
     @Ignore
     constructor(profileId: Int, id: Long, name: String, surname: String) {
         this.profileId = profileId
