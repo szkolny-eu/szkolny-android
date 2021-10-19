2476cd10120fc580ac06973ec169e57cd7b79d35
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2021-10-19 21:19:14 +0200

    [UI] Add new base dialog classes.

---
+++ app/src/main/java/pl/szczodrzynski/edziennik/ui/dialogs/base/BindingDialog.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) Kuba Szczodrzyński 2021-10-18.
+ */
+
+package pl.szczodrzynski.edziennik.ui.dialogs.base
+
+import android.view.LayoutInflater
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+
+abstract class BindingDialog<B : ViewBinding>(
+    activity: AppCompatActivity,
+    onShowListener: ((tag: String) -> Unit)? = null,
+    onDismissListener: ((tag: String) -> Unit)? = null,
+) : ViewDialog<View>(activity, onShowListener, onDismissListener) {
+
+    protected lateinit var b: B
+    protected abstract fun inflate(layoutInflater: LayoutInflater): B
+
+    final override fun getRootView(): View {
+        b = inflate(activity.layoutInflater)
+        return b.root
+    }
+}
---
+++ app/src/main/java/pl/szczodrzynski/edziennik/ui/dialogs/base/ConfigDialog.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) Kuba Szczodrzyński 2021-10-18.
+ */
+
+package pl.szczodrzynski.edziennik.ui.dialogs.base
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+import kotlinx.coroutines.launch
+import pl.szczodrzynski.edziennik.MainActivity
+import pl.szczodrzynski.edziennik.R
+
+abstract class ConfigDialog<B : ViewBinding>(
+    activity: AppCompatActivity,
+    private val reloadOnDismiss: Boolean = true,
+    onShowListener: ((tag: String) -> Unit)? = null,
+    onDismissListener: ((tag: String) -> Unit)? = null,
+) : BindingDialog<B>(activity, onShowListener, onDismissListener) {
+
+    final override fun getPositiveButtonText() = R.string.ok
+    final override suspend fun onShow() = Unit
+
+    protected val config by lazy { app.config.grades }
+
+    protected open suspend fun loadConfig() = Unit
+    protected open suspend fun saveConfig() = Unit
+    protected open fun initView() = Unit
+
+    final override suspend fun onBeforeShow(): Boolean {
+        initView()
+        loadConfig()
+        return true
+    }
+
+    final override fun onDismiss() {
+        launch {
+            saveConfig()
+        }
+        if (reloadOnDismiss && activity is MainActivity)
+            activity.reloadTarget()
+    }
+}
