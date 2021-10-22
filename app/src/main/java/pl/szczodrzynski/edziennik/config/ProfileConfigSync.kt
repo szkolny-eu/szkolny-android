e2ad3758e04ef18f7ce95aab000dde4d150cbd26
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2021-10-22 17:03:20 +0200

    [Config] Fix notification filter setting not loading.

--- app/src/main/java/pl/szczodrzynski/edziennik/config/ProfileConfigSync.kt
+++ app/src/main/java/pl/szczodrzynski/edziennik/config/ProfileConfigSync.kt
@@ -4,12 +4,12 @@
 
 package pl.szczodrzynski.edziennik.config
 
-import pl.szczodrzynski.edziennik.config.utils.get
+import pl.szczodrzynski.edziennik.config.utils.getIntList
 import pl.szczodrzynski.edziennik.config.utils.set
 
 class ProfileConfigSync(private val config: ProfileConfig) {
     private var mNotificationFilter: List<Int>? = null
     var notificationFilter: List<Int>
-        get() { mNotificationFilter = mNotificationFilter ?: config.values.get("notificationFilter", listOf()); return mNotificationFilter ?: listOf() }
+        get() { mNotificationFilter = mNotificationFilter ?: config.values.getIntList("notificationFilter", listOf()); return mNotificationFilter ?: listOf() }
         set(value) { config.set("notificationFilter", value); mNotificationFilter = value }
 }
