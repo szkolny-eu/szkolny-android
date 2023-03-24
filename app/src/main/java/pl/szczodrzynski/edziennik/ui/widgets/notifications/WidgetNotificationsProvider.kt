8177d4aa2d4150b43e1fb0f4b0973380344c0d1f
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2023-03-24 11:13:00 +0100

    [Widgets] Fix pending intents mutability. Hide timetable sync button.

--- app/src/main/java/pl/szczodrzynski/edziennik/ui/widgets/notifications/WidgetNotificationsProvider.kt
+++ app/src/main/java/pl/szczodrzynski/edziennik/ui/widgets/notifications/WidgetNotificationsProvider.kt
@@ -23,6 +23,7 @@ import pl.szczodrzynski.edziennik.R
 import pl.szczodrzynski.edziennik.ext.Bundle
 import pl.szczodrzynski.edziennik.ext.getJsonObject
 import pl.szczodrzynski.edziennik.ext.pendingIntentFlag
+import pl.szczodrzynski.edziennik.ext.pendingIntentMutable
 import pl.szczodrzynski.edziennik.ext.putExtras
 import pl.szczodrzynski.edziennik.receivers.SzkolnyReceiver
 import pl.szczodrzynski.edziennik.ui.base.enums.NavTarget
@@ -50,7 +51,7 @@ class WidgetNotificationsProvider : AppWidgetProvider() {
             val syncIntent = SzkolnyReceiver.getIntent(context, Bundle(
                     "task" to "SyncRequest"
             ))
-            val syncPendingIntent = PendingIntent.getBroadcast(context, 0, syncIntent, pendingIntentFlag())
+            val syncPendingIntent = PendingIntent.getBroadcast(context, 0, syncIntent, pendingIntentMutable())
             views.setOnClickPendingIntent(R.id.widgetNotificationsSync, syncPendingIntent)
 
             views.setImageViewBitmap(
@@ -71,13 +72,13 @@ class WidgetNotificationsProvider : AppWidgetProvider() {
 
             val itemIntent = Intent(context, MainActivity::class.java)
             itemIntent.action = Intent.ACTION_MAIN
-            val itemPendingIntent = PendingIntent.getActivity(context, 0, itemIntent, pendingIntentFlag())
+            val itemPendingIntent = PendingIntent.getActivity(context, appWidgetId, itemIntent, pendingIntentMutable())
             views.setPendingIntentTemplate(R.id.widgetNotificationsListView, itemPendingIntent)
 
             val headerIntent = Intent(context, MainActivity::class.java)
             headerIntent.action = Intent.ACTION_MAIN
             headerIntent.putExtras("fragmentId" to NavTarget.NOTIFICATIONS)
-            val headerPendingIntent = PendingIntent.getActivity(context, 0, headerIntent, pendingIntentFlag())
+            val headerPendingIntent = PendingIntent.getActivity(context, appWidgetId, headerIntent, pendingIntentMutable())
             views.setOnClickPendingIntent(R.id.widgetNotificationsHeader, headerPendingIntent)
 
             appWidgetManager.updateAppWidget(appWidgetId, views)
