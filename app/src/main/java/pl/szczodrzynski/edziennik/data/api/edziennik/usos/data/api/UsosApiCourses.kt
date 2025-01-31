514fbafd00a723c26bc6b171276d029dd69181be
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2025-01-31 19:16:03 +0100

    [API/Usos] Change team codes to include unique ID or current year

--- app/src/main/java/pl/szczodrzynski/edziennik/data/api/edziennik/usos/data/api/UsosApiCourses.kt
+++ app/src/main/java/pl/szczodrzynski/edziennik/data/api/edziennik/usos/data/api/UsosApiCourses.kt
@@ -9,6 +9,7 @@ import pl.szczodrzynski.edziennik.data.api.ERROR_USOS_API_INCOMPLETE_RESPONSE
 import pl.szczodrzynski.edziennik.data.api.edziennik.usos.DataUsos
 import pl.szczodrzynski.edziennik.data.api.edziennik.usos.ENDPOINT_USOS_API_COURSES
 import pl.szczodrzynski.edziennik.data.api.edziennik.usos.data.UsosApi
+import pl.szczodrzynski.edziennik.data.db.entity.GradeCategory
 import pl.szczodrzynski.edziennik.data.db.entity.Team
 import pl.szczodrzynski.edziennik.ext.*
 
@@ -25,17 +26,20 @@ class UsosApiCourses(
         apiRequest<JsonObject>(
             tag = TAG,
             service = "courses/user",
+            params = mapOf(
+                "active_terms_only" to false,
+            ),
             fields = listOf(
                 // "terms" to listOf("id", "name", "start_date", "end_date"),
                 "course_editions" to listOf(
                     "course_id",
                     "course_name",
-                    // "term_id",
                     "user_groups" to listOf(
                         "course_unit_id",
                         "group_number",
-                        // "class_type",
+                        "class_type",
                         "class_type_id",
+                        "term_id",
                         "lecturers",
                     ),
                 ),
@@ -63,22 +67,38 @@ class UsosApiCourses(
         for (courseEdition in courseEditions) {
             val courseId = courseEdition.getString("course_id") ?: continue
             val courseName = courseEdition.getLangString("course_name") ?: continue
-            val userGroups = courseEdition.getJsonArray("user_groups")?.asJsonObjectList() ?: continue
+            val userGroups =
+                courseEdition.getJsonArray("user_groups")?.asJsonObjectList() ?: continue
             for (userGroup in userGroups) {
                 val courseUnitId = userGroup.getLong("course_unit_id") ?: continue
                 val groupNumber = userGroup.getInt("group_number") ?: continue
-                // val classType = userGroup.getLangString("class_type") ?: continue
+                val classType = userGroup.getLangString("class_type") ?: continue
                 val classTypeId = userGroup.getString("class_type_id") ?: continue
+                val termId = userGroup.getString("term_id") ?: continue
                 val lecturers = userGroup.getLecturerIds("lecturers")
 
-                data.teamList.put(courseUnitId, Team(
-                    profileId,
-                    courseUnitId,
-                    "${profile?.studentClassName} $classTypeId$groupNumber - $courseName",
-                    2,
-                    "${data.schoolId}:${courseId} $classTypeId$groupNumber",
-                    lecturers.firstOrNull() ?: -1L,
-                ))
+                data.teamList.put(
+                    courseUnitId, Team(
+                        profileId,
+                        courseUnitId,
+                        "${profile?.studentClassName} $courseName ($classTypeId$groupNumber)",
+                        2,
+                        "${data.schoolId}:${termId}:${courseId} $classTypeId$groupNumber",
+                        lecturers.firstOrNull() ?: -1L,
+                    )
+                )
+
+                val gradeCategory = data.gradeCategories[courseUnitId]
+                data.gradeCategories.put(
+                    courseUnitId, GradeCategory(
+                        profileId,
+                        courseUnitId,
+                        gradeCategory?.weight ?: -1.0f,
+                        0,
+                        courseId,
+                    ).addColumn(classType)
+                )
+
                 hasValidTeam = true
             }
         }
